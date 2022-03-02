c2d56e10485d2cc8ed8c072e68a6cac8243f80df
canisluspussasha <aleksandra.trninic@stud.fh-campuswien.ac.at>
2022-03-02 12:12:50 +0100

    Sashas commit

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class AppTest {
     @Test
     public void myfirstTest(){
