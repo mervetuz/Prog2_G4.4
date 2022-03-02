9dc5280a287b873a2c037e4da9774bebc1bb5822
markus-wieger <markus.wieger@stud.fh-campuswien.ac.at>
2022-03-02 19:37:12 +0100

    all methods declared

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -1,4 +1,26 @@
 package at.ac.fhcampuswien;
 
 public class Article {
+    private String author = "";
+    private String title = "";
+
+    public Article(String author, String title){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() { //automatically created
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
