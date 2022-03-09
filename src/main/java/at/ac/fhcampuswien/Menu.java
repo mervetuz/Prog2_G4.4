9d68ebc7ef5c001ccf11ed578d49d49ff10ee1aa
markus-wieger <markus.wieger@stud.fh-campuswien.ac.at>
2022-03-09 16:17:00 +0100

    Worked on Menu.java

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,17 +1,27 @@
 package at.ac.fhcampuswien;
 
+import java.util.Scanner;
+
 public class Menu {
     private AppController controller = new AppController();
-    private static final String INVALID_INPUT_MESSAGE = "";
-    private static final String EXIT_MESSAGE = "";
+    private static final String INVALID_INPUT_MESSAGE = "Not a valid Input!";
+    private static final String EXIT_MESSAGE = "Bye bye!";
 
 
     public void start(){
         printMenu();
+        handleInput(new Scanner(System.in).next());
 
     }
 
     private void handleInput(String input){
+        switch (input) {
+            case "a" -> getTopHeadlinesAustria(controller);
+            case "b" -> getAllNewsBitcoin(controller);
+            case "y" -> getArticleCount(controller);
+            case "q" -> printExitMessage();
+            default -> printInvalidInputMessage();
+        }
 
     }
 
@@ -28,15 +38,16 @@ public class Menu {
     }
 
     private static void printExitMessage(){
+        System.out.println(EXIT_MESSAGE);
 
     }
 
     private static void printInvalidInputMessage(){
-
+        System.out.println(INVALID_INPUT_MESSAGE);
     }
 
     private static void printMenu(){
-
+        System.out.println("What do you want to do?");
 
     }
 
