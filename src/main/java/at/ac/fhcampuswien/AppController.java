a0833b1a0210e63c499b573ff5a0104d17f1da2c
Merve Tuz <mervetuz11@gmail.com>
2022-03-25 07:03:24 +0100

    Comments

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -13,14 +13,17 @@ public class AppController {
 
     public AppController(){
     }
-
+    //Setter for the Articles list
     public void setArticles(List<Article> articles) {
         this.articles = articles;
 
     }
+    //Returns the number of items in the list. If the list is null, 0 should be returned
+    public int getArticleCount(){
+        return 0;
+    }
 
-    public int getArticleCount(){return 0;}
-
+    //Should only return the list of Articles. If the list is null, an empty list should be returned
     public List<Article> getTopHeadlinesAustria(){
         return null;
     }
