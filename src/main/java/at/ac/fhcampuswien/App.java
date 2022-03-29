40b47e603a02ef6e0fb402acaefb61946e961f99
canisluspussasha <aleksandra.trninic@stud.fh-campuswien.ac.at>
2022-03-29 20:03:27 +0200

    javafx plugin

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,8 +1,8 @@
 package at.ac.fhcampuswien;
 
-public class App {
+public class App  {
     public static void main(String[] args){
-         Menu menu = new Menu();
-         menu.start();
+        Menu menu = new Menu();
+        menu.start();
     }
 }
