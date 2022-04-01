ac5346a2c1556718337eb64f679e02452f229de1
canisluspussasha <aleksandra.trninic@stud.fh-campuswien.ac.at>
2022-04-01 19:08:38 +0200

    cleaned up the test and added BeforeEach

--- src/test/java/at/ac/fhcampuswien/UnitTest.java
+++ src/test/java/at/ac/fhcampuswien/UnitTest.java
@@ -10,10 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class UnitTest {
+    private AppController ctrl;
+    private List<Article> articles;
+    private List<Article> acceptedList;
 
     @BeforeAll
     public static void init() {
-        System.out.println("Testing News-App");
+       System.out.println("Testing News-App");}
+
+    @BeforeEach
+    public void setup() {
+        ctrl = new AppController();
+        articles = new ArrayList<>();
     }
 
     @AfterAll
@@ -21,24 +29,21 @@ public class UnitTest {
         System.out.println("Finished Testing the News-App");
     }
 
-
-
     /**
      * Tests if the given List of Articles is assigned to the Classvariable articles in AppController.
      */
     @Test
+    @DisplayName("Testing setArticles Method")
     public void test_setArticles(){
-        System.out.println("Testing setArticles Method");
-        List<Article> testList = new ArrayList<Article>();
         Article first = new Article("Peter","Das ist ein Article");
         Article second = new Article("Hans","Das ist ein Article");
         Article third = new Article("Gustav","Das ist ein Article");
-        testList.add(first);
-        testList.add(second);
-        testList.add(third);
-        AppController ctrl = new AppController();
-        ctrl.setArticles(testList);
-        assertEquals(testList,ctrl.getArticles());
+
+        articles.add(first);
+        articles.add(second);
+        articles.add(third);
+        ctrl.setArticles(articles);
+        assertEquals(articles,ctrl.getArticles());
     }
 
     /**
@@ -48,9 +53,6 @@ public class UnitTest {
     @Test
     @DisplayName("No articles")
     public void test_getArticleCount_Scenario_0(){
-        AppController ctrl = new AppController();
-        List<Article> articles = new ArrayList<>();
-
         ctrl.setArticles(articles);
 
         int actual = ctrl.getArticleCount();
@@ -61,9 +63,6 @@ public class UnitTest {
     @Test
     @DisplayName("Number of articles")
     public void test_getArticleCount_Scenario_1(){
-        AppController ctrl = new AppController();
-        List<Article> articles = new ArrayList<>();
-
         Article first = new Article("Marco Polo", "Welcome to Venice!");
         articles.add(first);
         Article second = new Article("Christopher Columbus", "Hello America!");
@@ -82,9 +81,6 @@ public class UnitTest {
     @Test
     @DisplayName("Empty List")
     public void test_getTopHeadLinesAustria_Scenario_2(){
-        AppController ctrl = new AppController();
-        List<Article> articles = new ArrayList<>();
-
         Article first = new Article("El Salvador", "Tech this week: El Salvador rejects IMF call to drop Bitcoin use");
         articles.add(first);
         Article second = new Article("Ian Smith", "Bitcoin price crash: Crypto extends losses as market fails to rally");
@@ -101,18 +97,15 @@ public class UnitTest {
     //Getting all news with "bitcoin" in the title
     @DisplayName("Has bitcoin in the title, regardless of whether case sensitive or not")
     public void test_getAllNewsBitcoin_Scenario_1(){
-
-        List <Article> testList = new ArrayList<Article>();
-        List<Article> acceptedList = new ArrayList<Article>();
-        AppController ctrl = new AppController();
+        acceptedList = new ArrayList<Article>();
 
         Article first = new Article("Ian Smith", "Cryptos sind cool");
         Article second = new Article("Peter Smith", "Bitcoin ist cool");
         Article third = new Article("Franz Smith", "Cryptos sind königlich");
-        testList.add(first);
-        testList.add(second);
-        testList.add(third);
-        ctrl.setArticles(testList);
+        articles.add(first);
+        articles.add(second);
+        articles.add(third);
+        ctrl.setArticles(articles);
         acceptedList.add(second);
         assertEquals(acceptedList, ctrl.getAllNewsBitcoin());
 
@@ -125,17 +118,16 @@ public class UnitTest {
     public void test_filterList_Scenario_1(){
 
         String searchWord = "Ich";
-        List<Article> testList = new ArrayList<Article>();
-        List<Article> acceptedList = new ArrayList<Article>();
+        acceptedList = new ArrayList<Article>();
 
         Article first = new Article("Peter","Das ist ein Article");
         Article second = new Article("Hans","Ich bin ein Article");
         Article third = new Article("Gustav","Das ist ein Article");
-        testList.add(first);
-        testList.add(second);
-        testList.add(third);
+        articles.add(first);
+        articles.add(second);
+        articles.add(third);
         acceptedList.add(second);
-        assertEquals(acceptedList,AppController.filterList(searchWord,testList));
+        assertEquals(acceptedList,AppController.filterList(searchWord,articles));
 
     }
 
@@ -143,17 +135,16 @@ public class UnitTest {
     @DisplayName("Testing if case sensitive")
     public void test_filterList_scenario_2(){
         String searchWord = "ich";
-        List<Article> testList = new ArrayList<Article>();
-        List<Article> acceptedList = new ArrayList<Article>();
+        acceptedList = new ArrayList<Article>();
 
         Article first = new Article("Peter","Das ist ein Article");
         Article second = new Article("Hans","Ich bin ein Article");
         Article third = new Article("Gustav","Das ist ein Article");
-        testList.add(first);
-        testList.add(second);
-        testList.add(third);
+        articles.add(first);
+        articles.add(second);
+        articles.add(third);
         acceptedList.add(second);
-        assertEquals(acceptedList,AppController.filterList(searchWord,testList));
+        assertEquals(acceptedList,AppController.filterList(searchWord,articles));
     }
 
 }
