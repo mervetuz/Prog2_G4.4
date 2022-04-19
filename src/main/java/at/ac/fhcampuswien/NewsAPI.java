8179caa9b7220f702cdb0c8d0569d4bbff17ae20
AlexanderTomic <mirela.tomic35@gmail.com>
2022-04-19 16:37:49 +0200

    coding NewsAPI, getting articles from API

--- src/main/java/at/ac/fhcampuswien/NewsAPI.java
+++ src/main/java/at/ac/fhcampuswien/NewsAPI.java
@@ -1,5 +1,11 @@
 package at.ac.fhcampuswien;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class NewsAPI {
 
     /* Erstellt eine NewsApi Klasse, welche die Logik zum Senden/Empfangen von Requests/Responses, sowie die Erstellung der geeigneten URL (bspw. Endpoint und notwendige Queryparameter) implementiert. In weiterer Folge soll eure NewsApi Klasse von eurer AppController Klasse, mit den korrekten Queryparametern und Endpoints aufgerufen werden können. Achtung: der Parameter „q“ muss immer befüllt werden! (bei der Funktion getTopHeadlinesAustria() könnt ihr das Query frei wählen)
@@ -8,6 +14,26 @@ Bspw.:
 - AppController.getAllNewsBitcoin() -> sendet mithilfe der NewsApi folgenden Request: https://newsapi.org/v2/everything?q=bitcoin&apiKey=<YOUR_API_KEY>
 Achtet darauf, dass die Methoden der NewsApi Klasse so flexibel wie möglich sind. Es soll in Zukunft leicht möglich sein, neue Funktionen im AppController hinzuzufügen, die unterschiedliche Requestparameter senden.*/
 
+    final OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        NewsAPI example = new NewsAPI();
+        String response = example.run("https://newsapi.org/v2/top-headlines?country=at&apiKey=1c3a1d04cc674ddaa897818225da2afe");
+        System.out.println(response);
+
+    }
+
 
 
 }
+
