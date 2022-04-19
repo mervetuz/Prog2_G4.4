504ac0e8155cfd9e0f9bd1566c17336e27669069
AlexanderTomic <mirela.tomic35@gmail.com>
2022-04-19 15:54:25 +0200

    added okhttp, gson, coded NewsResponse class, created NewsAPI class

---
+++ src/main/java/at/ac/fhcampuswien/NewsAPI.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampuswien;
+
+public class NewsAPI {
+
+    /* Erstellt eine NewsApi Klasse, welche die Logik zum Senden/Empfangen von Requests/Responses, sowie die Erstellung der geeigneten URL (bspw. Endpoint und notwendige Queryparameter) implementiert. In weiterer Folge soll eure NewsApi Klasse von eurer AppController Klasse, mit den korrekten Queryparametern und Endpoints aufgerufen werden können. Achtung: der Parameter „q“ muss immer befüllt werden! (bei der Funktion getTopHeadlinesAustria() könnt ihr das Query frei wählen)
+Bspw.:
+- AppController.getTopHeadlinesAustria() -> sendet mithilfe der NewsApi folgenden Request: https://newsapi.org/v2/top-headlines?q=corona&apiKey=<YOUR_API_KEY>&country=at
+- AppController.getAllNewsBitcoin() -> sendet mithilfe der NewsApi folgenden Request: https://newsapi.org/v2/everything?q=bitcoin&apiKey=<YOUR_API_KEY>
+Achtet darauf, dass die Methoden der NewsApi Klasse so flexibel wie möglich sind. Es soll in Zukunft leicht möglich sein, neue Funktionen im AppController hinzuzufügen, die unterschiedliche Requestparameter senden.*/
+
+
+
+}
