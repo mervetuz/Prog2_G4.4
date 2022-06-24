afc0e5c457437b4d1ff4a5a7038b004b47cb5ae3
AlexanderTomic <mirela.tomic35@gmail.com>
2022-06-24 19:45:20 +0200

    push

--- src/main/java/at/ac/fhcampuswien/models/Article.java
+++ src/main/java/at/ac/fhcampuswien/models/Article.java
@@ -51,20 +51,3 @@ public class Article {
                 "Content: " + content + "\n" ;
     }
 }
-class Source{
-    private final String id;
-    private final String name;
-
-    Source(String id,String name){
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
---
+++ src/main/java/at/ac/fhcampuswien/models/Source.java
@@ -0,0 +1,19 @@
+package at.ac.fhcampuswien.models;
+
+public class Source {
+    private final String id;
+    private final String name;
+
+    Source(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
